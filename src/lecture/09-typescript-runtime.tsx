27beaa54c8867f3e45a5fae01490d2335ec295aa
ksyee <tjssud0647@gmail.com>
2024-01-22 10:35:52 +0900

    [learn] exercise part 3

--- src/lecture/09-typescript-runtime.tsx
+++ src/lecture/09-typescript-runtime.tsx
@@ -1,5 +1,5 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
 
 // @ts-ignore
 const { createRoot } = ReactDOM;
@@ -13,7 +13,7 @@ const createApp = () => {
   );
 };
 
-const rootElement = document.querySelector("#root") as HTMLDivElement;
+const rootElement = document.querySelector('#root') as HTMLDivElement;
 const root = createRoot(rootElement);
 
 root.render(createApp());
