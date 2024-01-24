4b14621ed0c27492049d140a777819b0333511f2
ksyee <tjssud0647@gmail.com>
2024-01-24 14:13:26 +0900

    Refactor mapping over data in Exercise component

--- src/lecture/25-mapping-over-data.jsx
+++ src/lecture/25-mapping-over-data.jsx
@@ -5,33 +5,20 @@ import contactData from '../data/contacts.json';
 //        문(statement)
 //        for of 문을 사용할 수 있나요? (O)
 
-for (const item of contactData) {
-  console.log(item);
-}
-
-console.log('---------------------------');
-
-// 식(expression)
-// forEach, map, filter, ...
-
-contactData.items.forEach((item) => {
-  console.log(item);
-});
-
-// map (리액트에서 많이 사용하는 배열 이터레이터)
-const mapResult = contactData.items.map((item) => {
-  return item;
-});
-
-console.log(mapResult);
-
-// filter
-const filteredResult = contactData.items.filter((item) => {
-  return item.name === '박선호';
-});
-
-console.log(filteredResult);
-
 export default function Exercise() {
-  return null;
+  const { item } = contactData;
+  const result = item.map(({ id, name, email }, index) => {
+    return (
+      <li style={listStyle} key={index}>
+        <span>{id}</span> <span>{name}</span> <span>{email}</span>
+      </li>
+    );
+  });
+  return <ul>{result}</ul>;
 }
+
+const listStyle = {
+  display: 'flex',
+  flexFlow: 'column',
+  marginBlockEnd: 20,
+};
