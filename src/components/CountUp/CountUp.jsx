1b8b4b5302e8eb69868387d03a261d83f2ddf43b
ksyee <tjssud0647@gmail.com>
2024-01-29 15:04:29 +0900

    Add CountUp component with styling

--- src/components/CountUp/CountUp.jsx
+++ src/components/CountUp/CountUp.jsx
@@ -1,5 +1,14 @@
+import classes from './CountUp.module.css';
+
 function CountUp({ count = 0 }) {
-  return <div>Count up {count}</div>;
+  return (
+    <div className={classes.CountUp}>
+      <output>{count}</output>
+      <button type="button" className={classes.button}>
+        +1
+      </button>
+    </div>
+  );
 }
 
 export default CountUp;
