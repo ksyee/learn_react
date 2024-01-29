5b4131eb2a0d690a123da3301ce7cc66c0c49f43
ksyee <tjssud0647@gmail.com>
2024-01-29 13:22:43 +0900

    Update Graph.stories.js with new args

--- src/components/Graph/Graph.stories.js
+++ src/components/Graph/Graph.stories.js
@@ -8,4 +8,20 @@ const metaConfig = {
 
 export default metaConfig;
 
-export const Base = {};
+export const Base = {
+  args: {
+    start: 10,
+    end: 20,
+  },
+};
+
+Base.storyName = '1-5까지 그래프';
+
+export const Second = {
+  args: {
+    start: 5,
+    end: 20,
+  },
+};
+
+Second.storyName = '5-20(5)까지 그래프';
