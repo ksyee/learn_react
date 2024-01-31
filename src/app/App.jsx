d13cf876bd6c71d220c1f9b483ce30b3ec16f015
ksyee <tjssud0647@gmail.com>
2024-02-01 08:57:39 +0900

    Refactor CSS import and add new CSS module

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,5 +1,7 @@
-import './App.css';
+import { app as appClassName } from './App.module.css';
 import Exercise from '@/lecture/37-3-react-form-exercise.jsx';
+
+console.log(appClassName);
 
 function App() {
   return (
