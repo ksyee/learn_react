92d0801a3543e5762a2e8d9a3a6a5ea541bb2ee7
ksyee <tjssud0647@gmail.com>
2024-01-31 10:06:40 +0900

    Refactor AccordionPanel component to use CSS modules

--- src/lecture/36-lifting-state-up.jsx
+++ src/lecture/36-lifting-state-up.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import { A11yHidden } from '../components';
+import classes from './36-lifting-state-up.module.css';
 
 // [학습 주제]
 // 컴포넌트 사이에 상태를 공유해요!
@@ -9,13 +10,10 @@ import { A11yHidden } from '../components';
 // - 컴포넌트 사이에 상태를 공유하려면?
 //   공유하려는 컴포넌트 들의 가장 가까운 상위 컴포넌트로 상태를 끌어올려야 한다.
 
-function AccordionPanel({ children }) {
-  const [isOpen, setIsOpen] = useState(false);
-  const handleTogglePanel = () => setIsOpen(!isOpen);
-
+function AccordionPanel({ isOpen = false, onToggle, children }) {
   return (
-    <div className="">
-      <button type="button" onClick={handleTogglePanel}>
+    <div className={classes.AccordionPanel}>
+      <button type="button" onClick={onToggle}>
         {isOpen ? '닫음' : '열림'}
       </button>
       <div hidden={!isOpen}>{children}</div>
@@ -25,16 +23,29 @@ function AccordionPanel({ children }) {
 
 function Accordion() {
   const headlineId = crypto.randomUUID();
+  const [openedPanelIndex, setOpenedPanelIndex] = useState(0);
+
+  const handleOpenPanel = (panelIndex) => {
+    setOpenedPanelIndex(panelIndex);
+  };
 
   return (
-    <article className="" aria-labelledby={headlineId}>
+    <article className={classes.Accordion} aria-labelledby={headlineId}>
       <A11yHidden as="h2" id={headlineId}>
         아코디언을 사용해 컴포넌트 간 상태 공유
       </A11yHidden>
-      <AccordionPanel>
+      <AccordionPanel
+        data-index={0}
+        isOpen={openedPanelIndex === 0}
+        onToggle={handleOpenPanel}
+      >
         <p>아코디언 컴포넌트는 ..... 1</p>
       </AccordionPanel>
-      <AccordionPanel>
+      <AccordionPanel
+        data-index={1}
+        isOpen={openedPanelIndex === 1}
+        onToggle={handleOpenPanel}
+      >
         <p>아코디언 컴포넌트는 ..... 2</p>
       </AccordionPanel>
     </article>
