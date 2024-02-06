0eb4079c92dbea8bd2f6074da96a1228c137cbcd
ksyee <tjssud0647@gmail.com>
2024-02-06 13:38:29 +0900

    실습 코드

--- src/lecture/42-1-external-lib-dom-control.jsx
+++ src/lecture/42-1-external-lib-dom-control.jsx
@@ -1,22 +1,36 @@
 import { useTilt } from '@/hooks';
 import { range } from '@/utils';
+import { Tilt } from 'react-tilt';
 
+const BOX_COUNT = 9;
 const BOX_OPTIONS = {
   reverse: true,
   glare: true,
   'max-glare': 0.7,
-  scale: 0.94,
 };
 
 function Exercise() {
   return (
-    <div className="flex gap-2">
-      {range(10, 160, 2).map((n) => (
-        <TiltBox key={n} options={BOX_OPTIONS}>
-          {n}
-        </TiltBox>
-      ))}
-    </div>
+    <>
+      <div className="flex gap-2">
+        {range(1, BOX_COUNT, 2).map((n, i) => (
+          <TiltBox key={i} options={BOX_OPTIONS}>
+            {n}
+          </TiltBox>
+        ))}
+      </div>
+      <div>
+        {range(40, 90, 7).map((n) => {
+          <Tilt
+            key={n}
+            className="bg-black h-250pxr w-250pxr"
+            options={BOX_OPTIONS}
+          >
+            <span>🏠</span>
+          </Tilt>;
+        })}
+      </div>
+    </>
   );
 }
 
