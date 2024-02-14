dfd44392cd45304019439ecf484d2cdcd9477c52
ksyee <tjssud0647@gmail.com>
2024-02-14 13:04:31 +0900

    실습 코드

--- src/pages/DeferredValue.jsx
+++ src/pages/DeferredValue.jsx
@@ -1,6 +1,9 @@
 import Exercise from '@/lecture/deferred-value/Exercise-01';
+import { useDocumentTitle } from '@/hooks';
 
 function DeferredValuePage() {
+  useDocumentTitle('Deferred Value');
+
   return (
     <>
       <h2 className="my-5">상태 업데이트 지연</h2>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,9 @@
+import { Helmet } from 'react-helmet-async';
+
 function HomePage() {
   return (
     <>
+      <Helmet>홈 ← </Helmet>
       <h2 className="my-5">홈 페이지</h2>
     </>
   );
